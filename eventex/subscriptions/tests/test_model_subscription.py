61d649c9a205bd6e9d08ce14b863fc9b5996d234
ricardo <beckert@DESKTOP-5LSHH6H.localdomain>
2019-10-03 19:37:39 -0300

    Campo paid

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -21,3 +21,7 @@ class SubscriptionModelTest(TestCase):
     
     def test_str(self):
         self.assertEqual('Ricardo Beckert', str(self.obj))
+
+    def test_paid_default_to_false(self):
+        """By default paid must be false"""
+        self.assertEqual(False, self.obj.paid)
