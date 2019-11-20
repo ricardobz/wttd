bc6b4efa10bf5f74ce41e75b7f0dfd6983674e71
ricardo <beckert@DESKTOP-5LSHH6H.localdomain>
2019-11-20 14:28:00 -0200

    Captalize Name

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -19,6 +19,12 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(cpf='1234')
         self.assertFormErrorCode(form, 'cpf', 'length')
 
+    def test_name_must_be_captalized(self):
+        """Name must be captalized"""
+        # RICARDO beckert -> Ricardo Beckert
+        form = self.make_validated_form(name='RICARDO beckert')
+        self.assertEqual('Ricardo Beckert', form.cleaned_data['name'])
+
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
         errors_list = errors[field]
